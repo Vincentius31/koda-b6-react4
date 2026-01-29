ef493be8949ccf466c591e391ce29294be4ce802
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 09:20:47 +0700

    feat: add about page that using navbar, about, adn footer component

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../components/Navbar"
+import { AboutUs } from "../components/AboutUs"
+import Footer from "../components/footer"
+
+export const About = () =>{
+    return (
+        <>
+            <Navbar/>
+            <AboutUs/>
+            <Footer/>
+        </>
+    )
+}
