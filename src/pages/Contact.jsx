4da4faf18159c7babb2e267486c250aad0709f3f
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 09:21:31 +0700

    feat: add contact page that using navbar, aboutus, footer component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import { ContactUs } from "../components/ContactUs"
+import Footer from "../components/footer"
+import { Navbar } from "../components/Navbar"
+
+export const Contact = () =>{
+    return (
+        <>
+            <Navbar/>
+            <ContactUs/>
+            <Footer/>
+        </>
+    )
+}
