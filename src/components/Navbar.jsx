d2f8819a3fdd27c6aac03ff40bbfea8adc70d722
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-01-29 09:18:15 +0700

    feat: adding routing for the navbar using react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <header className="flex items-center justify-between px-8 py-6">
       <h1 className="font-serif tracking-wide">Gourmet au Catering</h1>
       <ul className="flex gap-8 text-gray-700">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </ul>
     </header>
   );
